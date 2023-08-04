37e1332149f8f3cc250837a3d3b5dc0650f57bb2
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2023-08-05 03:50:17 +0530

    Deploy b8661705f8a6157a8ff249362c7585d2afb92445

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ddccebfbf862c4d2728114afeb338ee1",
-"index.html": "bbb7d7e56118990822a9085508f304ee",
-"/": "bbb7d7e56118990822a9085508f304ee",
+"index.html": "bee02cb7417803b582e57eb2195128e6",
+"/": "bee02cb7417803b582e57eb2195128e6",
 "main.dart.js": "67039650708506554a1d9cb368e9d2bb",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
